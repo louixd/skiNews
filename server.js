6ca625dbc4f9da99f2fdc69cac628448ef5a0ea6
louixd <louis.poleym@gmail.com>
2026-09-21 13:38:14 +0000

    feat: donnees en memoire + route GET liste

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+let articles = [
+  { id: 1, nom: "Présentation des nouveaux skis Faction", categorie: "Freeride" },
+  { id: 2, nom: "Découverte des skis K2", categorie: "Freestyle" },
+  { id: 3, nom: "Rossignol, une révolution ?", categorie: "Freeride" }
+];
 
 // Route de test : GET /
 app.get("/", (req, res) => {
-  res.json({ message: "Mon API fonctionne" });
+  res.json(articles);
 });
-
+// GET /produits -> renvoie tout le tableau
+app.get("/articles", (req, res) => {
+  res.json(articles);
+});
 // On demarre le serveur sur le port 3000
 app.listen(3000, () => {
   console.log("Server sur http://localhost:3000");
